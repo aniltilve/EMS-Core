2e4085c4d9246da8301d36c069e3ee48df3058ab
Anil <Anil@DESKTOP-PE3EO5G>
2018-01-10 18:05:05 -0500

    Added stored procedure to add and update tblHospital

---
+++ EMS.DbSchema/dbo/Stored Procedures/uspSaveHospital.sql
@@ -0,0 +1,59 @@
+﻿CREATE PROCEDURE [dbo].[uspSaveHospital]
+(
+	@iHospitalID INT = NULL OUTPUT,
+	@iAgencyID INT,
+	@szName VARCHAR(100),
+	@bIsActive BIT,
+	@szInsertedBy VARCHAR(64),
+	@szUpdatedBy VARCHAR(64) = NULL
+)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@iHospitalID IS NULL OR @iHospitalID <= 0)
+		BEGIN
+			INSERT INTO 
+				dbo.tblHospital
+			(
+				iAgencyID,
+				szName,
+				bIsActive,
+				dtInserted,
+				szInsertedBy
+			)
+			VALUES
+			(
+				@iAgencyID,
+				@szName,
+				1,
+				GETDATE(),
+				@szInsertedBy
+			)
+			SELECT @iHospitalID = @@IDENTITY
+		END
+		ELSE
+		BEGIN
+			UPDATE dbo.tblHospital
+			SET
+				szName = @szName,
+				bIsActive = @bIsActive,
+				dtUpdated = GETDATE(),
+				szUpdatedBy = @szUpdatedBy
+			WHERE
+				iHospitalID = @iHospitalID
+		END
+	END TRY
+	BEGIN CATCH
+        DECLARE @errorMessage VARCHAR(256)
+
+        SELECT
+            @errorMessage =
+            'Number = ' + CAST(ERROR_NUMBER() AS VARCHAR(8)) + ' ' +
+            'Severity = ' + CAST(ERROR_SEVERITY() AS VARCHAR(8)) + ' ' +
+            'Procedure = ' + ERROR_PROCEDURE() + ' ' +
+            'Line = ' + CAST(ERROR_LINE() AS VARCHAR(8)) + ' ' +
+            'Message = ' + ERROR_MESSAGE() + ' '
+
+        RAISERROR (@errorMessage, 16, 1)
+    END CATCH
+END
