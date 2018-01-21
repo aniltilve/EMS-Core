fd8de7d57f9bbde0acbcd37aa86beb434c2aab3d
Anil <Anil@DESKTOP-PE3EO5G>
2018-01-21 18:14:57 -0500

    Found way to specify Sqlparameter direction without if statement

--- BusinessLayer/Agency.cs
+++ BusinessLayer/Agency.cs
@@ -18,30 +18,6 @@ namespace EMS.Business
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
 
-                //SqlParameter iAgencyID = new SqlParameter("@iAgencyID", agency.iAgencyID);
-                //iAgencyID.Direction = System.Data.ParameterDirection.Output;
-
-                //command.Parameters.Add(iAgencyID);
-                //command.Parameters.Add(new SqlParameter("@szName", agency.szName));
-                //command.Parameters.Add(new SqlParameter("@szShortName", agency.szShortName));
-                //command.Parameters.Add(new SqlParameter("@szEIN", agency.szEIN));
-                //command.Parameters.Add(new SqlParameter("@szRegState", agency.szRegState));
-                //command.Parameters.Add(new SqlParameter("@szStatus", agency.szStatus));
-                //command.Parameters.Add(new SqlParameter("@dtSubscribeStart", agency.dtSubscribeStart));
-                //command.Parameters.Add(new SqlParameter("@dtSubscribeEnd", (object)agency.dtSubscribeEnd ?? DBNull.Value));
-                //command.Parameters.Add(new SqlParameter("@bTaxExempt", agency.bTaxExempt));
-                //command.Parameters.Add(new SqlParameter("@mLicensePrice", agency.mLicensePrice));
-                //command.Parameters.Add(new SqlParameter("@mRenewalPrice", agency.mRenewalPrice));
-                //command.Parameters.Add(new SqlParameter("@szInsertedBy", agency.szInsertedBy));
-                //command.Parameters.Add(new SqlParameter("@szUpdatedBy", (object)agency.szUpdatedBy ?? DBNull.Value));
-                //command.Parameters.Add(new SqlParameter("@szNotificationEmail", agency.szNotificationEmail));
-                //command.Parameters.Add(new SqlParameter("@bNotificationRequired", agency.bNotificationRequired));
-                //command.Parameters.Add(new SqlParameter("@bAgreement", agency.bAgreement));
-                //command.Parameters.Add(new SqlParameter("@dtAgreementAccepted", (object)agency.dtAgreementAccepted ?? DBNull.Value));
-                //command.Parameters.Add(new SqlParameter("@iAgreementAcceptedUserID", (object)agency.iAgreementAcceptedUserID ?? DBNull.Value));
-                //command.Parameters.Add(new SqlParameter("@szIPAddress", agency.szIPAddress));
-                //command.Parameters.Add(new SqlParameter("@szLogoFileName", agency.szLogoFileName));
-
                 PropertyInfo[] propertyInfo = agency.GetType().
                     GetProperties().
                     Where(property => property.Name != "bIsActive"
@@ -51,13 +27,13 @@ namespace EMS.Business
 
                 foreach (PropertyInfo property in propertyInfo)
                 {
-                    SqlParameter parameter = new SqlParameter(String.Format("@{0}", property.Name), (property.GetValue(agency) ?? DBNull.Value));
-
-                    if (parameter.ParameterName.Equals("@iAgencyID"))
-                        parameter.Direction = System.Data.ParameterDirection.Output;
+                    SqlParameter parameter = new SqlParameter(String.Format("@{0}", property.Name), 
+                        (property.GetValue(agency) ?? DBNull.Value));
 
                     command.Parameters.Add(parameter);
                 }
+
+                command.Parameters["@iAgencyID"].Direction = System.Data.ParameterDirection.Output;
 
                 agencyID = new DataRepository<Agency>().Add(command);
             }
