14b5dce4ee17baab5228c32822b78c5b2621adc1
Anil <Anil@DESKTOP-PE3EO5G>
2018-05-31 20:21:54 -0400

    Added US States entity

---
+++ Entities/USState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Entity
+{
+    class USState
+    {
+        public string szStateName { get; set; }
+
+        [Key]
+        public string szAbbrev { get; set; }
+    }
+}
